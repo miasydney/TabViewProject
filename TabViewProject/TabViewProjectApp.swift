efd8546bfae22357371c605435425346551b2ef7
mia <miabenn74@gmail.com>
2023-04-14 17:37:32 +1000

    Initial Commit

---
+++ TabViewProject/TabViewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewProjectApp.swift
+//  TabViewProject
+//
+//  Created by Mia on 14/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct TabViewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
